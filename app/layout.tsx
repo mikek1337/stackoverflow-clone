231d92d34880d67584d1974d952af86159b56adc
mikek1337 <mikekegnlyew@gmail.com>
2023-09-03 23:07:45 +0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { DM_Sans } from 'next/font/google';
+
+const dmSans = DM_Sans({
+  subsets:['latin']
+})
+
+export const metadata: Metadata = {
+  title: 'Stackoverflow-clone',
+  description: 'simple project for alx',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}){
+  
+  return (
+    <html lang="en">
+      <body className={dmSans.className}>{children}</body>
+    </html>
+  )
+}
