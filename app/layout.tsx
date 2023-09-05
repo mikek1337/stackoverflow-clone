a03812b9755f1aeb2f51915368d7e08ff3b7ee24
Mikek1337 <mikekegnlyew@gmail.com>
2023-09-05 17:12:06 +0300

    stat

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,33 @@
-import NavBar from '@/components/navbar';
-import './globals.css'
-import type { Metadata } from 'next'
-import { DM_Sans } from 'next/font/google';
+import NavBar from "@/components/navbar";
+import "./globals.css";
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import Providers from "@/components/providers";
 
 const dmSans = DM_Sans({
-  subsets:['latin']
-})
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'Stackoverflow-clone',
-  description: 'simple project for alx',
-}
+  title: "Stackoverflow-clone",
+  description: "simple project for alx",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
-}){
-  
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      
       <body className={dmSans.className}>
-        <NavBar/>
-        {children}
+        <Providers>
+          <div className="container">
+            <NavBar />
+            {children}
+          </div>
+        </Providers>
       </body>
     </html>
-  )
+  );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,35 +1,32 @@
-'use client'
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { Input } from "./ui/input";
-import { Button } from "./ui/button";
-import {signIn} from 'next-auth/react'
-const NavBar = ()=> {
+import { buttonVariants } from "./ui/button";
+const NavBar = () => {
   return (
-   <div className="container pt-2">
-        <ul className="flex justify-center items-center gap-4">
-            <li className="w-fit ">
-                <div className="w-40 h-fit">
-                    <Image src="/logo.svg" alt="logo" width="10" height="20" />
-                </div>
-            </li>
-            <li className="w-fit">
-            <div>
-                <Link href="#">About</Link>
-            </div>
-            </li>
-            <li className=" md:w-full w-98">
-                <Input type="search" placeholder="Search..."/>
-            </li>
-            <li className="flex gap-1 justify-end w-fit">
-                <Button  className="w-20 " onClick={()=>signIn()}>Login</Button>
-                <Button variant="secondary" className="w-20">Signup</Button>
-            </li>
-        </ul>
+    <div className="container pt-2">
+      <ul className="flex justify-center items-center gap-4">
+        <li className="w-fit ">
+          <div className="w-40 h-fit">
+            <Image src="/logo.svg" alt="logo" width="10" height="20" />
+          </div>
+        </li>
+        <li className="w-fit">
+          <div>
+            <Link href="#">About</Link>
+          </div>
+        </li>
+        <li className=" md:w-full w-20">
+          <Input type="search" placeholder="Search..." />
+        </li>
+        <li className="flex gap-1 justify-end w-fit">
+            <Link href="/login" className={buttonVariants({variant:'default'})}>Login</Link>
+            <Link href="/signup" className={buttonVariants({variant:'ghost'})}>SignUp</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-      
-   </div>
-  )
-}
-
-export default NavBar
+export default NavBar;
