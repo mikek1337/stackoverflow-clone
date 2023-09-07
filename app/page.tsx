d5cf9597f539fcda27a5399d595f1b79fc3134a7
mikek1337 <mikekegnlyew@gmail.com>
2023-09-07 22:10:01 +0300

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+
 export default function Home() {
   return (
-    <div className="container mx-auto flex w-full flex-col justify-center  space-y-6 sm:w-[800px] ">
-      <div className="flex flex-col space-y-2 text-center">
+    <div className="container mx-auto flex w-full flex-col justify-center top-72 relative space-y-6 sm:w-[800px] ">
+      <div className="flex flex-col space-y-2 text-center gap-2">
         <h1 className="sm:text-5xl text-2xl font-bold text-center">
           Welcome to Stackoverflow clone
         </h1>
@@ -12,7 +13,7 @@ export default function Home() {
           Every developer data scientist system admin mobile developer game
           developer has a tab open to Stack Overflow
         </p>
-        <Button className={cn("w-[300px] mx-auto")}>Explore</Button>
+        <Button className={cn("w-[500px] mx-auto ")}>Get Started</Button>
       </div>
     </div>
   );
