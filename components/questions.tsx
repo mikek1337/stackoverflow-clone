6bf68462eabaf030c3f3a20b9415fb1415ab823d
Mikek1337 <mikekegnlyew@gmail.com>
2023-09-13 16:43:11 +0300

    fixed question layout

--- components/questions.tsx
+++ components/questions.tsx
@@ -13,21 +13,24 @@ interface questionsProps {
 const Questions: FC<questionsProps> = ({ questionType, ...props }) => {
   return (
     <div className="container">
-      <div className="flex">
-        <div className="flex flex-col text-zinc-300 w-fit text-sm">
+      <div className="flex justify-center items-center">
+        <div className="flex flex-col text-zinc-300 w-24 text-xs gap-1">
           <span>0 votes</span>
           <span>0 answers</span>
           <span>3 views</span>
         </div>
-        <div>
+        <div className="flex flex-col ">
           <Link
             href="/questions/1"
-            className={buttonVariants({ variant: "link" })}
+            className={cn(
+              buttonVariants({ variant: "link" }),
+              "text-left w-fit"
+            )}
           >
             question title
           </Link>
-          <div className="flex justify-between">
-            <Badge variant="outline" className="bg-blue-100 w-fit text-sm">
+          <div className="flex justify-between items-center">
+            <Badge variant="outline" className="bg-blue-100 w-fit text-sm ">
               tag
             </Badge>
             <UserAvatar user={{ name: "name", image: "" }} />
